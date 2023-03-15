1bf1c551c38727dec2e3376db5175887ff02d9d8
DrLucasPCD <115511395+DrLucasPCD@users.noreply.github.com>
2023-03-14 21:01:49 -0300

    Add files via upload

---
+++ meu site/function redirectToApps() {.js
@@ -0,0 +1,8 @@
+function redirectToApps() {
+    window.location.href = "https://drive.google.com/drive/folders/1E89yjvT34TKA_7P8wUAROAMSJ1i729hH?usp=sharing";
+  }
+  
+  function redirectToBooks() {
+    window.location.href = "https://www.amazon.com.br/kindle-dbs/entity/author/B0BRJS2VXW?_encoding=UTF8&node=8561254011&offset=0&pageSize=12&searchAlias=stripbooks&sort=author-sidecar-rank&page=1&langFilter=default#formatSelectorHeader";
+  }
+  
